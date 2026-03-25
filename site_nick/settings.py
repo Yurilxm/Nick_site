7b106f229a7903ea955a1211005a5ac08d72babb
Yurilxm <yurilxm@yahoo.com.br>
2026-03-25 15:37:56 -0300

    fix: corrige backend do Cloudinary

--- site_nick/settings.py
+++ site_nick/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cloudinary',
-    'storages',
     'app',
     'produtos',
     'carrinho',
@@ -182,7 +181,7 @@ cloudinary.config(
 
 STORAGES = {
     "default": {
-        "BACKEND": "storages.backends.cloudinary.CloudinaryStorage",
+        "BACKEND": "cloudinary.storage.CloudinaryMediaStorage",
     },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
